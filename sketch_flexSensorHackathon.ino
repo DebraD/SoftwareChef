fd70cef4eb76dc1fa811f3e25975ef204a0339df
Debra Do <debdo@rocketmail.com>
2013-10-05 18:05:06 -0700

    LCD & Buzzer

--- sketch_flexSensorHackathon.ino
+++ sketch_flexSensorHackathon.ino
@@ -63,6 +63,7 @@ Version 2.0 6/2012 MDG
 // Include the servo library to add servo-control functions:
 
 // #include <Servo.h> 
+#include <LiquidCrystal.h>
 
 // Create a servo "object", called servo1. Each servo object
 // controls one servo (you can have a maximum of 12):
@@ -72,13 +73,47 @@ Version 2.0 6/2012 MDG
 // Define the analog input pin to measure flex sensor position:
 
 const int flexpin = 0; 
+const int buzzerPin = 9;
+const int normalRange = 255;
+const int forwardRange = 20;
+const int backwardRange = 20;
+const int outOfRangeTime = 5; // in seconds 
+int counter = 0;
 
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+// Length must equal the total number of notes and spaces 
+
+const int songLength = 18;
+
+// Notes is an array of text characters corresponding to the notes
+// in your song. A space represents a rest (no tone)
+
+char notes[] = "cdfda ag cdfdg gf "; // a space represents a rest
+
+// Beats is an array of values for each note and rest.
+// A "1" represents a quarter-note, 2 a half-note, etc.
+// Don't forget that the rests (spaces) need a length as well.
+
+int beats[] = {1,1,1,1,1,1,4,4,2,1,1,1,1,1,1,4,4,2};
+
+// The tempo is how fast to play the song.
+// To make the song play faster, decrease this value.
+
+int tempo = 150;
 
 void setup() 
 { 
   // Use the serial monitor window to help debug our sketch:
    
   Serial.begin(9600);
+  
+  pinMode(13, OUTPUT); // led
+  pinMode(buzzerPin, OUTPUT); // buzzer
+  
+  lcd.begin(16, 2);
+  lcd.clear();
+ // lcd.print("hello, world");
   
   // Enable control of a servo on pin 9:
 
@@ -113,7 +148,32 @@ void loop()
   // print out our values to the serial monitor window:
   
   Serial.print("sensor: ");
-  Serial.print(flexposition);
+  Serial.println(flexposition);
+  
+  if (flexposition > normalRange + 20 || flexposition < normalRange - 20)
+  {
+    counter = counter + 1;
+    Serial.println(counter);
+    
+    if (counter > 5)
+    {
+    //  Serial.println("Red light on");
+      digitalWrite(13, HIGH);
+      
+      if (counter > 10)
+      {
+        lcd.print("Straighten up!");
+        playSong();
+      }
+    }
+  }
+  else
+  {
+    counter = 0;
+  //  Serial.println("Red light off");
+    digitalWrite(13, LOW);
+  }
+  
  // Serial.print("  servo: ");
  // Serial.println(servoposition);
   
@@ -129,7 +189,59 @@ void loop()
   // 600 and 900 in the map() function above, you'll exactly match
   // the flex sensor's range with the servo's range.
   
-  delay(20);  // wait 20ms between servo updates
+  delay(1000);  // wait 1 second between servo updates
 } 
 
+void playSong()
+{
+  int i, duration;
+  
+  for (i = 0; i < songLength; i++) // step through the song arrays
+  {
+    duration = beats[i] * tempo;  // length of note/rest in ms
+    
+    if (notes[i] == ' ')          // is this a rest? 
+    {
+      delay(duration);            // then pause for a moment
+    }
+    else                          // otherwise, play the note
+    {
+      tone(buzzerPin, frequency(notes[i]), duration);
+      delay(duration);            // wait for tone to finish
+    }
+    delay(tempo/10);              // brief pause between notes
+  }
+}
 
+int frequency(char note) 
+{
+  // This function takes a note character (a-g), and returns the
+  // corresponding frequency in Hz for the tone() function.
+  
+  int i;
+  const int numNotes = 8;  // number of notes we're storing
+  
+  // The following arrays hold the note characters and their
+  // corresponding frequencies. The last "C" note is uppercase
+  // to separate it from the first lowercase "c". If you want to
+  // add more notes, you'll need to use unique characters.
+
+  // For the "char" (character) type, we put single characters
+  // in single quotes.
+
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int frequencies[] = {262, 294, 330, 349, 392, 440, 494, 523};
+  
+  // Now we'll search through the letters in the array, and if
+  // we find it, we'll return the frequency for that note.
+  
+  for (i = 0; i < numNotes; i++)  // Step through the notes
+  {
+    if (names[i] == note)         // Is this the one?
+    {
+      return(frequencies[i]);     // Yes! Return the frequency
+    }
+  }
+  return(0);  // We looked through everything and didn't find it,
+              // but we still need to return a value, so return 0.
+}
